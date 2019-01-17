78bc648a6cc092e8176c82df813ee04607d644af
Jared Harbison <jared.harbison@gmail.com>
2019-01-17 16:10:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,9 +16,10 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z].*\.$/) == nil 
-    return false
+    false
   else 
-    return true 
+    true 
+  end
 end
 
 def valid_phone_number?(phone)
